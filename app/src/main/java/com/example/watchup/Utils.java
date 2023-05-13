d009cd6db7e58c485dcc370e5ec6b29bd9088380
Luca Daia <87373703+LucaZeu@users.noreply.github.com>
2023-05-13 16:44:14 +0300

    recyclerView bun,  inainte de onClick

--- app/src/main/java/com/example/watchup/Utils.java
+++ app/src/main/java/com/example/watchup/Utils.java
@@ -7,11 +7,18 @@ import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class Utils {
-    public static String BaseUrl = "http://10.0.2.2:8081/api/";
+//    public static String BaseUrl = "http://10.0.2.2:8081/api/";
+      public static String BaseUrl = "https://caca-188-26-139-100.ngrok-free.app/api/";
 
     public static void insertImageByName(String name, ImageView imageView) {
         String url = Utils.BaseUrl + "images/name/" + name;
@@ -44,6 +51,24 @@ public class Utils {
         } else {
             return ""; // Return an empty string if "Images\" is not found
         }
+    }
+
+
+    public static void sortObjectsByDate(List<Image> objects) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+        Collections.sort(objects, new Comparator<Image>() {
+            @Override
+            public int compare(Image obj1, Image obj2) {
+                String dateStr1 = obj1.getCreatedAt();
+                String dateStr2 = obj2.getCreatedAt();
+
+                LocalDateTime date1 = LocalDateTime.parse(dateStr1, formatter);
+                LocalDateTime date2 = LocalDateTime.parse(dateStr2, formatter);
+
+                return date2.compareTo(date1);
+            }
+        });
     }
 }
 
